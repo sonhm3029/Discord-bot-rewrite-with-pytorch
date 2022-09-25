b5abcd1f5ea5fc0452ce85044ffbb7d0ebdda32d
Son Hoang <hoangminhson29102001@gmail.com>
2022-09-25 12:18:28 +0700

    thêm logging và info

--- bot.py
+++ bot.py
@@ -1,15 +1,35 @@
 import discord
 import os
 import json
+import time
+import asyncio
 
 # discord bot token
 token = os.environ['token']
+server_id = os.environ['server_id']
 
 intents = discord.Intents.default()
 intents.message_content = True
-
+messages = joined = 0
 
 client = discord.Client(intents=intents)
+
+
+async def update_stats():
+    await client.wait_until_ready()
+    global messages, joined
+
+    while not client.is_closed():
+        try:
+            with open("stats.txt", "a") as f:
+                f.write(f"Time: {int(time.time())}, Messages: {messages}, Members joined: {joined}\n")
+            messages = 0
+            joined = 0
+
+            await asyncio.sleep(5)
+        except Exception as e:
+            print(e)
+            await asyncio.sleep(5)
 
 
 @client.event
@@ -18,9 +38,34 @@ async def on_ready():
 
 
 @client.event
+async def on_member_join(member):
+    global joined
+    joined += 1
+    for channel in member.server.channels:
+        if str(channel) == "chung":
+            await client.send_message(f"""Welcome to my server {member.mention}""")
+
+
+@client.event
 async def on_message(msg):
-    if msg.content.find("!hello") != -1:
-        await msg.channel.send("Hi")
+    global messages
+    messages += 1
+    guide = client.get_guild(int(server_id))
+    channels = ["chung"]
+    valid_users = ["sonhm#7995"]
+
+    if str(msg.channel) in channels:
+        if msg.content.find("$hello") != -1:
+            await msg.channel.send("Hi!")
+        elif msg.content == "$users":
+            await msg.channel.send(f"""# of Members {guide.member_count}""")
+    else:
+        print(f"""User: {msg.author} tried to do command {msg.content}, in channel {msg.channel}""")
+
+
+@client.event
+async def setup_hook():
+    client.loop.create_task(update_stats())
 
 
 client.run(token)
